1179c33a367dedb62f8449d0e950f43c5e847d1f
Anthony Stoltzfus <anthonystoltzfus@gmail.com>
2022-04-05 01:02:32 +0800

    fix error

--- db.js
+++ db.js
@@ -15,7 +15,7 @@ const proConfig = {
 }
 
 export const pool = new Pool(
-    process.env.NODE_ENV == "production" ? proConfig : devConfig
+    process.env.NODE_ENV === "production" ? proConfig : devConfig
 );
 
 //module.exports = pool;
