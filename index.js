3bae574ce30770f2ccd84173868b43e89a922038
Anthony Stoltzfus <anthonystoltzfus@gmail.com>
2022-04-05 01:37:23 +0800

    refresh cache

--- index.js
+++ index.js
@@ -22,8 +22,6 @@ app.use(cors());
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  //server static content
-  //npm run build
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 
